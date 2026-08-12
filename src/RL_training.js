fd7b72da64b57d6e62938c8c189aa901b273518a
vinitkumarsingh632-creator <vinitkumarsingh632@gmail.com>
2026-08-12 17:12:43 +0530

    started the project

---
+++ src/RL_training.js
@@ -0,0 +1,25 @@
+const Q = {}
+
+
+const alpha = 0.5 // learning rate
+const gamma = 0.9 //discount factor
+const epsilon = 0.2 // Exploration
+
+const winningCombination = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
+]
+
+function checkAvailableMoves (board) {
+  
+}
+
+let episodes = 0
+while (episodes < 200){
+  const board = [
+    "","","",
+    "","","",
+    "","",""
+  ]
+}
