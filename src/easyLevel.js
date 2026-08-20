07b5142d7fdc4109aab91a35359d616d4f19475e
vinitkumarsingh632-creator <vinitkumarsingh632@gmail.com>
2026-08-20 20:26:29 +0530

    added winning logic

--- src/easyLevel.js
+++ src/easyLevel.js
@@ -1,5 +1,6 @@
 const grids = document.querySelectorAll(".gridParent div");
 let move = null
+const decisionPanel = document.getElementById('decisionPanel')
 
 const firstMove = Math.random()
 if(firstMove > 0.5){
@@ -29,33 +30,71 @@ function availableMoves() {
   return avlMoves;
 }
 
+function checkWinner() {
+    let X = [];
+    let O = [];
+
+    for (let items of grids) {
+        if (items.textContent === "X") {
+            X.push(Number(items.className));
+        }
+        else if (items.textContent === "O") {
+            O.push(Number(items.className));
+        }
+    }
+
+    for (let combination of winningCombination) {
+
+        if (combination.every(position => X.includes(position))) {
+            return "X Win";
+        }
+
+        if (combination.every(position => O.includes(position))) {
+            return "O Win";
+        }
+    }
+
+    return null;
+}
+
 // Adding Click Event
 if(move == 'bot'){
     const avlMoves = availableMoves()
     const botMove = Math.floor(Math.random() * avlMoves.length)
-    const moves = grids[botMove].textContent = 'O'
+    const moves = grids[Math.max(avlMoves[botMove] - 1, 0)].textContent = 'O'
     move = 'user'
 }
+
+function checkDraw () {
+  const avlMoves = availableMoves()
+  if(avlMoves.length == 0) return 'draw'
+}
 const gridParent = document.getElementsByClassName("gridParent")[0];
-gridParent.addEventListener("click", (event) => {
+const clickListener = gridParent.addEventListener("click", (event) => {
   const target = event.target;
   console.log(move)
   if(move == 'user'){
     console.log('User')
     const avlMoves = availableMoves()
-    if(avlMoves.includes(target.className)){
-         target.textContent = 'X';
-     move = 'bot'
+    const avl = avlMoves.includes(target.className)
+    if(!avl) {
+      alert('Already filled')
+      return;
     }
-    else{
-        alert('Already Filled')
-    }
+     target.textContent = "X";
+        move = "bot";
+
+     if(checkWinner() == 'X Win'){
+      decisionPanel.style.display = 'block'
+      removeEventListener('click',clickListener)
+      return
+     }
     setTimeout(()=>{
          const avlMoves = availableMoves()
     const botMove = Math.floor(Math.random() * avlMoves.length)
     console.log(avlMoves)
     console.log(botMove)
-    const moves = grids[botMove].textContent = 'O'
+    const moves = grids[Math.max(avlMoves[botMove] - 1,0)].textContent = 'O'
     move = 'user'
     },1000)
   }
